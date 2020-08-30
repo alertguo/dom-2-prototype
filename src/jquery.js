33dead023373ea08889dcc973603c047970df95b
alertguo <1196614612@qq.com>
2020-08-30 17:20:52 +0800

    添加append

--- src/jquery.js
+++ src/jquery.js
@@ -1,15 +1,55 @@
-const { fn } = require('jquery')
+// const { fn } = require('jquery')
 
-window.jQuery = function (selectorOrArray) {
+window.$ = window.jQuery = function (selectorOrArrayOrTemplate) {
   let elements
-  if (typeof selectorOrArray === 'string') {
-    elements = document.querySelectorAll(selectorOrArray)
-    //jQuery并不返回该元素elements，而是创造一个可以操作对应元素的对象(API)
-  } else if (selectorOrArray instanceof Array) {
-    elements = selectorOrArray
+  if (typeof selectorOrArrayOrTemplate === 'string') {
+    if (selectorOrArrayOrTemplate[0] === '<') {
+      //如果为标签，创建 div
+      elements = [createElement(selectorOrArrayOrTemplate)]
+    } else {
+      //查找 div
+      elements = document.querySelectorAll(selectorOrArrayOrTemplate)
+    }
+  } else if (selectorOrArrayOrTemplate instanceof Array) {
+    elements = selectorOrArrayOrTemplate
   }
+  //jQuery并不返回该元素elements，而是创造一个可以操作对应元素的对象(API)
+  function createElement(string) {
+    const container = document.createElement('template')
+    container.innerHTML = string.trim()
+    // trim() 会从一个字符串的两端删除空白字符
+    return container.content.firstChild
+  }
+  // api 可以操作 elements
   return {
     //闭包，函数访问外部变量
+    jquery: true,
+    elements: elements,
+    get(index) {
+      return elements[index]
+    },
+    appendTo(node) {
+      if (node instanceof Element) {
+        this.each((el) => node.appendChild(el))
+        // 遍历 elements，对每个 el 进行 node.appendChild 操作
+      } else if (node.jQuery === true) {
+        this.each((el) => node.get(0).appendChild(el))
+        // 遍历 elements，对每个 el 进行 node.get(0).appendChild(el)) 操作
+      }
+      return this
+    },
+    append(children) {
+      if (children instanceof Element) { // 一个节点
+        this.get(0).appendChild(children)
+      } else if (children instanceof HTMLCollection) { // HTML元素
+        for (let i = 0; i < children.length; i++) {
+          this.get(0).appendChild(children[i])
+        }
+      } else if (children.jquery === true) {
+        children.each((node) => this.get(0).appendChild(node))
+      }
+      return this
+    },
     find(selector) {
       let array = []
       for (let i = 0; i < elements.length; i++) {
@@ -22,7 +62,7 @@ window.jQuery = function (selectorOrArray) {
       // this 就是旧 api,也就是 jQuery()
       return jQuery(array)
     },
-    oldApi: selectorOrArray.oldApi, // 将oldApi 传出来给 end 使用
+    oldApi: selectorOrArrayOrTemplate.oldApi, // 将oldApi 传出来给 end 使用
     end() {
       return this.oldApi
       // this 是新 api,也就是 jQuery().find()
@@ -51,7 +91,7 @@ window.jQuery = function (selectorOrArray) {
       return jQuery(array)
     },
     print() {
-      console.log(elements)
+      console.log(elements) // elements 就是对应的元素们
     },
     addClass(className) {
       for (let i = 0; i < elements.length; i++) {
@@ -62,5 +102,3 @@ window.jQuery = function (selectorOrArray) {
     },
   }
 }
-
-window.$ = window.jQuery
